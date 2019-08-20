2c4e988d695d18ba9b64db77e87e6f4213ab6ef0
Braeden Mulligan <braeden.mulligan@gmail.com>
2019-08-19 17:11:34 -0700

    Crude infrastructure for user input.

--- generator.c
+++ generator.c
@@ -1,19 +1,28 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 //TODO: do smoothing
 
-void square(int** h_map, int row, int col, int chunk_size) {
+typedef struct map_params {
+	int map_size;
+	int amplitude;
+	int smoothing;
+} map_container_t;
+
+void square(int** h_map, int row, int col, int chunk_size, int amp_scale) {
 	float sum = h_map[row - chunk_size / 2][col - chunk_size / 2];
 	sum += h_map[row - chunk_size / 2][col + chunk_size / 2];
 	sum += h_map[row + chunk_size / 2][col - chunk_size / 2];
 	sum += h_map[row + chunk_size / 2][col + chunk_size / 2];
 	float avg = sum / 4;
 
-    h_map[row][col] = (int)avg; //TODO: + perturbance.
+	h_map[row][col] = ((int) avg) + ((rand() - (RAND_MAX / 2)) % amp_scale);
 }
 
-void diamond(int** h_map, int row, int col, int chunk_size, int map_size) {
+void diamond(int** h_map, int row, int col, int chunk_size, int map_size, int amp_scale) {
 	int sum = 0;
 	int pts = 0;
 
@@ -35,63 +44,87 @@ void diamond(int** h_map, int row, int col, int chunk_size, int map_size) {
 	};  
 	
 	float avg = sum / pts;
-	h_map[row][col] = (int)avg; //TODO: + perturbance.
+	h_map[row][col] = ((int) avg) + ((rand() - (RAND_MAX / 2)) % amp_scale);
 }
 
-void map_generate(int** h_map, int map_size) {
-	int chunk_size = map_size - 1;
+// amplitude determines how tall/jaggen map is.
+void map_generate(int** h_map, map_container_t map_params) {
+	srand(time(0));
+
+	int chunk_size = map_params.map_size - 1;
+	int amp_scale = map_params.amplitude;
 	while (chunk_size > 1) {
 
 		// chunk_size should be power of 2.
 		int half_chunk = chunk_size / 2; 
 
-		for (int r = half_chunk; r < map_size; r += chunk_size) {
-			for (int c = half_chunk; c < map_size; c += chunk_size) {
-				square(h_map, r, c, chunk_size);
+		for (int r = half_chunk; r < map_params.map_size; r += chunk_size) {
+			for (int c = half_chunk; c < map_params.map_size; c += chunk_size) {
+				square(h_map, r, c, chunk_size, amp_scale);
 			}
 		}
 
-		for (int r = 0; r < map_size; r += half_chunk) {
-			for (int c = (r + half_chunk) % chunk_size; c < map_size; c += chunk_size) {
-				diamond(h_map, r, c, chunk_size, map_size);
+		for (int r = 0; r < map_params.map_size; r += half_chunk) {
+			for (int c = (r + half_chunk) % chunk_size; c < map_params.map_size; c += chunk_size) {
+				diamond(h_map, r, c, chunk_size, map_params.map_size, amp_scale);
 			}
 		}
 
 		chunk_size /= 2;
+		amp_scale /= 2;
 	}
 //TODO: trim map
 }
 
-int main() {
-
-	//TODO: get from input
-	int n = 33; 
-	int* height_map[n];
-	for (int i = 0; i < n; ++i) {
-		height_map[i] = malloc(n * sizeof(int));
+int main(int argc, char* argv[]) {
+	// TODO: getopt()
+	// Check to get parameters from arguments or from window objects.
+	bool gui = false;
+	for (int i = 0; i < argc; ++i) {
+		if (!strcmp(argv[i], "-g")) gui = true; 
 	}
 
-	/*
-	int** height_map = malloc(n * sizeof(int*));
-	for (int i = 0; i < n; ++i) {
-		height_map[i] = malloc(n * sizeof(int));
-	}
-	*/
+	int dimension = 2;
+	int height = 0;
+	int noise = 0;
+	if (gui) {
+		//TODO: get params from window
+	}else {
+		// Debug
+		for (int i = 1; i < argc; ++i) {
+			if (!strcmp(argv[i], "-d")) dimension = atoi(argv[i + 1]);
+			if (!strcmp(argv[i], "-h")) height = atoi(argv[i + 1]); 
+			if (!strcmp(argv[i], "-n")) noise = atoi(argv[i + 1]); 
+		}
+	};
+	map_container_t map_params = {.map_size = dimension,
+		.amplitude = height,
+		.smoothing = noise };
 
+	int** height_map = malloc(dimension * sizeof(int*));
+	for (int i = 0; i < dimension; ++i) {
+		height_map[i] = malloc(dimension * sizeof(int));
+	}
 	//TODO: get from input
 	height_map[0][0] = 20;
-	height_map[0][n - 1] = 15;
-	height_map[n - 1][0] = 10;
-	height_map[n - 1][n - 1] = 5;
-	map_generate(height_map, n);
+	height_map[0][dimension - 1] = 15;
+	height_map[dimension  - 1][0] = 10;
+	height_map[dimension - 1][dimension - 1] = 5;
+
+	map_generate(height_map, map_params);
 	
 	// Display map.
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			printf("%2d ", height_map[i][j]);
-		};
-		printf("\n");
-	}
+	if (gui) {
+		//SDL2
+	else{ 
+		for (int i = 0; i < dimension; ++i) {
+			for (int j = 0; j < dimension; ++j) {
+				printf("%3d", height_map[i][j]);
+			};
+			printf("\n");
+		}
+	};
+
 	return 0;
 }
 
